0443410581f2bce6a03ed9cabb7c0320f7337d5c
LIM YI CHWEN <yichwenlim@gmail.com>
2018-11-07 22:42:02 +0800

    The Net Ninja - OAuth Login (Passport.js) Tutorial #15

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,17 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
+const User = require('../models/user-model');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+});
 
 passport.use(
     new GoogleStrategy({
@@ -9,7 +20,22 @@ passport.use(
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
     }, (accessToken, refreshToken, profile, done) => {
-        // passport callback function
-        console.log(profile);
+        // check if user already exists in our db
+        User.findOne({ googleId: profile.id }).then((currentUser) => {
+            if(currentUser) {
+                // already have the user
+                console.log('user is ' + currentUser);
+                done(null, currentUser);
+            } else {
+                // if not, create user in our db
+                new User({
+                    username: profile.displayName,
+                    googleId: profile.id
+                }).save().then((newUser) => {
+                    console.log('new user created ' + newUser);
+                    done(null, newUser);
+                });
+            }
+        });
     })
 );
