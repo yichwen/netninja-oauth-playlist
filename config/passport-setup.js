81625a0c633fdef75ef89e3fd10ce342f1f3d918
LIM YI CHWEN <ben.lim@solderfieldgroup.com>
2018-11-08 10:37:52 +0800

    The Net Ninja - OAuth Login (Passport.js) Tutorial #21

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -24,15 +24,14 @@ passport.use(
         User.findOne({ googleId: profile.id }).then((currentUser) => {
             if(currentUser) {
                 // already have the user
-                console.log('user is ' + currentUser);
                 done(null, currentUser);
             } else {
                 // if not, create user in our db
                 new User({
                     username: profile.displayName,
-                    googleId: profile.id
+                    googleId: profile.id,
+                    thumbnail: profile._json.image.url
                 }).save().then((newUser) => {
-                    console.log('new user created ' + newUser);
                     done(null, newUser);
                 });
             }
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -9,7 +9,8 @@ router.get('/login', (req, res) => {
 // auth logout
 router.get('/logout', (req, res) => {
     // handle with passport
-    res.send('logging out');
+    req.logout();
+    res.redirect('/');
 });
 
 // auth with google+
@@ -19,7 +20,8 @@ router.get('/google', passport.authenticate('google', {
 
 // callback route for google to redirect to
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-    res.send('you reached the callback URI');
+    // res.send(req.user);
+    res.redirect('/profile');
 });
 
 module.exports = router;
